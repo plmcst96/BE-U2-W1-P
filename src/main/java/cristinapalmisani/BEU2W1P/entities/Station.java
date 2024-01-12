70f8772978c12da91918c8fd9445d656d58a0e96
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-12 15:06:53 +0100

    fine creazione dao

---
+++ src/main/java/cristinapalmisani/BEU2W1P/entities/Station.java
@@ -0,0 +1,41 @@
+package cristinapalmisani.BEU2W1P.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "stations")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Station {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
+    private UUID univocCode;
+    private String description;
+    private StationType stationType;
+    private int numMaxOccupants;
+    @OneToMany(mappedBy = "station")
+    @ToString.Exclude
+    private List<Reservation> reservations;
+    @ManyToOne
+    @JoinColumn(name = "building_id")
+    private Building building;
+    @ManyToMany
+    @JoinTable(name = "station_user",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "reservation_id"))
+    private List<User> users;
+
+    public Station(String description, StationType stationType, int numMaxOccupants) {
+        this.description = description;
+        this.stationType = stationType;
+        this.numMaxOccupants = numMaxOccupants;
+    }
+}
