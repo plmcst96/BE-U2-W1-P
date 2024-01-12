2610d805c739678aa71af489d8be4b4c8a0a914e
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-12 10:56:28 +0100

    inizio progetto

---
+++ src/test/java/cristinapalmisani/BEU2W1P/BeU2W1PApplicationTests.java
@@ -0,0 +1,13 @@
+package cristinapalmisani.BEU2W1P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W1PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
